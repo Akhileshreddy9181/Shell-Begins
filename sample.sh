2fe88bf0106e8ad9555a3b461f83049930ff2e80
akhil <akhileshreddy9181@gmail.com>
2023-02-28 23:46:18 -0500

    Added Exit Status to script

--- sample.sh
+++ sample.sh
@@ -3,4 +3,4 @@ echo -e Hello World
 
 #Print multiple lines
 
-echo The components in roboshop are : \n cart\n shipping \n payment\n user\n dispatch
+echo "The components in roboshop are : \n cart\n shipping \n payment\n user\n dispatch"
