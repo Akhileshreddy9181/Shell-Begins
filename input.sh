c149d18a58dfce189cb2a1cc3f72035abb76bb61
akhil <akhileshreddy9181@gmail.com>
2023-02-28 23:56:11 -0500

    Added Exit Status to script

---
+++ input.sh
@@ -0,0 +1,3 @@
+read -p "Enter your Project Name:" pName
+
+echo "Your Project Name is :$pName"
--- sample.sh
+++ sample.sh
@@ -1,6 +1,5 @@
 #Enabling colour
-echo -e Hello World
+echo -e "\e[31mHello World \e[0m"
 
 #Print multiple lines
-
 echo -e "The components in roboshop are : \n cart\n shipping \n payment\n user\n dispatch"
